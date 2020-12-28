60524054b50264cf116046789aad743bdaeae873
SUpQuark <supquark@qq.com>
2020-12-28 11:27:27 +0800

    Change License

--- __init__.py
+++ __init__.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
-__all__=["init2"]
+__all__=["init"]
 __NAME="Gomoku"
 __AUTHOR="SUpQuark"
-__LICENSE="GPLv3"
+__LICENSE="MIT"
 
 if __name__ == "__main__":
     print("%s\nby %s\n%s Licensed."%(__NAME,__AUTHOR,__LICENSE) )
-    import print_map
-    import init2
-    print(init2())
-    exit()
+    #import print_map
+    import init
+    
     __PLAYING_MODE_LIST=(
             {'num':0,'mean':'PVE Player First'},
             {'num':1,'mean':'PVE Computer First'}
         )
-    while True:
-        #try:
-            for i in __PLAYING_MODE_LIST:
-                print("%d %s"%(i['num'],i['mean']) )
-            __PLAYING_MODE=int(input("Choose a playing mode: "))
-        #except:
-        #break
     
+    for i in __PLAYING_MODE_LIST:
+        print("%d %s"%(i['num'],i['mean']) )
+    __PLAYING_MODE=int(input("Choose a playing mode: "))    
     
-    #try:
-        init.init()
+    init.init()
     #except:
 
     #try:
-        if(__PLAYING_MODE%2==0):
-            user_input()
-            __PLAYING_MODE
-    while True:
-        
+    if(__PLAYING_MODE%2==0):
+        user_input()
+        __PLAYING_MODE
+    #while True:
+
